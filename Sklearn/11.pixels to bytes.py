f38d6c6e4f421a588c4e83743b6cace03c6f5c57
Emre-Akpinar <153773928+Emre-Akpinar@users.noreply.github.com>
2024-08-30 23:20:48 +0300

    Sklearn
    Sklearn file organized

--- Sklearn/11.pixels to bytes.py
+++ Sklearn/11.pixels to bytes.py
@@ -2,7 +2,7 @@ from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 
-img = Image.open("eightorzero.png")
+img = Image.open("10.eightorzero.png")
 data = list(img.getdata())
 
 for i in range(len(data)):
